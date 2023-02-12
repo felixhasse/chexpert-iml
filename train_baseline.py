998db791de52b161015ac922478fcacdb90a6d4a
Felix Hasse <felix.hasse@student.kit.edu>
2023-02-12 16:14:51 +0100

    Update train baseline file

--- train_baseline.py
+++ train_baseline.py
@@ -8,7 +8,6 @@ from constants import *
 from models import DenseNet121
 from chexpert_trainer import *
 from torch.utils.tensorboard import SummaryWriter
-from uuid import uuid4
 
 with open(CONFIG_PATH, "r") as file:
     config = json.load(file)
@@ -17,24 +16,31 @@ timestamp = round(time.time())
 
 model_path = f"models/baseline/{timestamp}"
 
-writer = SummaryWriter(logdir="runs/timestamp")
+writer = SummaryWriter(log_dir="runs/baseline/timestamp")
+with open("runs/baseline/timestamp/config.json", "w") as file:
+    json.dump(config, file)
+
+for key in config:
+    writer.add_text(tag=key, text_string=config[key])
+
 
 # Define list of image transformations
 transformation_list = [
     transforms.Resize((config["image_size"], config["image_size"])),
     transforms.ToTensor(),
-    transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD),
 ]
+
+if config["pretrained"]:
+    transformation_list.append(transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD),)
 
 image_transformation = transforms.Compose(transformation_list)
 
-
 train_dataset = CheXpertDataset(data_path="./data/CheXpert-v1.0-small/train.csv",
-                          uncertainty_policy=config["policy"], transform=image_transformation)
+                                uncertainty_policy=config["policy"], transform=image_transformation)
 
 test_dataset = CheXpertDataset(data_path="./data/CheXpert-v1.0-small/valid.csv",
-                          uncertainty_policy=config["policy"], transform=image_transformation)
-                          
+                               uncertainty_policy=config["policy"], transform=image_transformation)
+
 train_dataloader = DataLoader(dataset=train_dataset, batch_size=config["batch_size"], shuffle=True)
 test_dataloader = DataLoader(dataset=test_dataset, batch_size=config["batch_size"], shuffle=True)
 
@@ -51,10 +57,10 @@ model = DenseNet121(num_classes=1).to(device)
 loss_function = nn.BCELoss()
 
 # Adam optimizer
-optimizer = optim.Adam(model.parameters(), lr=config["lr"], betas=tuple(config["betas"]), eps=config["eps"], weight_decay=config["weight_decay"])
+optimizer = optim.Adam(model.parameters(), lr=config["lr"], betas=tuple(config["betas"]), eps=config["eps"],
+                       weight_decay=config["weight_decay"])
 
 # Best AUROC value during training
-best_score = 0
 training_losses = []
 validation_losses = []
 validation_score = []
@@ -63,13 +69,10 @@ validation_score = []
 mb = master_bar(range(config["epochs"]))
 mb.names = ['Training loss', 'Validation loss', 'Validation AUROC']
 x = []
-
-nonimproved_epoch = 0
 start_time = time.time()
 
 # Training each epoch
 for epoch in mb:
-    mb.main_bar.comment = f'Best AUROC score: {best_score}'
     x.append(epoch)
 
     # Training
@@ -79,27 +82,22 @@ for epoch in mb:
     training_losses.append(train_loss)
 
     # Evaluating
-    val_loss, new_score = evaluate(epoch, model, test_dataloader, device, loss_function, mb)
+    val_loss, score = evaluate(epoch, model, test_dataloader, device, loss_function, mb)
     writer.add_scalar("Validation loss", val_loss, epoch)
-    writer.add_scalar("ROCAUC", new_score, epoch)
-    mb.write('Finish validation epoch {} with loss {:.4f} and score {:.4f}'.format(epoch, val_loss, new_score))
+    writer.add_scalar("ROCAUC", score, epoch)
+    writer.flush()
+    mb.write('Finish validation epoch {} with loss {:.4f} and score {:.4f}'.format(epoch, val_loss, score))
     validation_losses.append(val_loss)
-    validation_score.append(new_score)
+    validation_score.append(score)
 
     # Update training chart
     mb.update_graph([[x, training_losses], [x, validation_losses], [x, validation_score]], [0, epoch + 1], [0, 1])
 
-    # Save model
-    if best_score < new_score:
-        mb.write(f"Improve AUROC from {best_score} to {new_score}")
-        best_score = new_score
-        torch.save({"model": model.state_dict(),
-                    "optimizer": optimizer.state_dict(),
-                    "best_score": best_score,
-                    "epoch": epoch,
-                    }, model_path)
-    
-writer.flush()
+    mb.write(f"AUROC: {score}")
+    torch.save({"model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "score": score,
+                "epoch": epoch,
+                }, model_path)
+
 writer.close()
-
-
