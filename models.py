bf4642208fe2cafa556fc28ce91618991cfd8312
Felix Hasse <felix.hasse@student.kit.edu>
2023-02-16 11:04:18 +0100

    Add MaxViT as alternative baseline model [EXPERIMENTAL, WIP]

--- models.py
+++ models.py
@@ -2,12 +2,32 @@ import torchvision
 from torch import nn
 
 
+class MaxViT(nn.Module):
+    def __init__(self, num_classes, pretrained=True):
+
+        super().__init__()
+        self.net = torchvision.models.maxvit_t(torchvision.models.MaxVit_T_Weights.DEFAULT if pretrained else None)
+
+        # Get the input dimension of last layer
+        kernel_count = self.net.classifier.in_features
+
+        # Replace last layer with new layer that have num_classes nodes, after that apply Sigmoid to the output
+        self.net.classifier = nn.Sequential(nn.Linear(kernel_count, num_classes), nn.Sigmoid())
+
+    def forward(self, inputs):
+        """
+        Forward the network with the inputs
+        """
+        return self.net(inputs)
+
+
 class DenseNet121(nn.Module):
     def __init__(self, num_classes, pretrained=True):
         super().__init__()
 
         # Load the DenseNet121 from ImageNet
-        self.net = torchvision.models.densenet121(pretrained=pretrained)
+        self.net = torchvision.models.densenet121(
+            weights=torchvision.models.DenseNet121_Weights.DEFAULT if pretrained else None)
 
         # Get the input dimension of last layer
         kernel_count = self.net.classifier.in_features
