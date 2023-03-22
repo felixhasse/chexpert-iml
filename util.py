c583568118d0e6f836232be40ad4f534c0e86fd3
Felix Hasse <felix.hasse@student.kit.edu>
2023-03-22 16:12:18 +0100

    Add util methods for generating bounding boxes and loading segmentation models

---
+++ util.py
@@ -0,0 +1,54 @@
+import torch
+
+from models import DeepLabV3ResNet50
+
+
+def calculate_diameter(mask: torch.Tensor):
+    # Creates a new 1d-tensor that represents the columns in the image
+    # Is True for each index if the corresponding column is part of the mask (contains white pixels), 0 otherwise
+    column_in_mask = torch.any(mask, dim=1).squeeze()
+
+    # Find left and right edge of the mask
+    nonzero_column_indexes = torch.nonzero(column_in_mask).squeeze()
+    # TODO: Find a better way to handle nonexistent masks
+    if len(nonzero_column_indexes) == 0:
+        return len(column_in_mask)
+    left_edge_index = nonzero_column_indexes[0].item()
+    right_edge_index = nonzero_column_indexes[-1].item()
+
+    diameter = right_edge_index - left_edge_index
+
+    return diameter
+
+
+def calculate_ctr(heart_mask: torch.Tensor, lung_mask: torch.Tensor):
+    cardiac_diameter = calculate_diameter(heart_mask)
+    thoracic_diameter = calculate_diameter(lung_mask)
+
+    ctr = cardiac_diameter / thoracic_diameter
+
+    return ctr
+
+
+def generate_bb(mask: torch.Tensor):
+    # Creates a new 1d-tensor that represents the columns/rows in the image
+    # Is True for each index if the corresponding column/row is part of the mask (contains white pixels), 0 otherwise
+    column_in_mask = torch.any(mask, dim=1).squeeze()
+    row_in_mask = torch.any(mask, dim=-1).squeeze()
+
+    # Find the edges of the bounding box
+    x_min = torch.nonzero(column_in_mask).squeeze()[0].item()
+    x_max = torch.nonzero(column_in_mask).squeeze()[-1].item()
+    y_min = torch.nonzero(row_in_mask).squeeze()[0].item()
+    y_max = torch.nonzero(row_in_mask).squeeze()[-1].item()
+
+    return x_min, x_max, y_min, y_max
+
+
+def load_segmentation_model(model_path: str, device: str):
+    model = DeepLabV3ResNet50(num_classes=1, pretrained=False)
+    state_dict = torch.load(model_path, map_location=torch.device(device))["model"]
+    model.load_state_dict(state_dict)
+    model = model.to(device)
+    model.eval()
+    return model
