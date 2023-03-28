eaff2f4618532ae6f5591176c4e7267dfa8b4b8a
Felix Hasse <felixhasse2000@gmail.com>
2023-03-28 18:40:19 +0200

    Update segment_chexpert script to also generate masks for validation set

--- segment_chexpert.py
+++ segment_chexpert.py
@@ -43,10 +43,15 @@ transformation_list = [
 ]
 image_transformation = transforms.Compose(transformation_list)
 
-dataset = CheXpertDataset(data_path="data/CheXpert-v1.0-small/train.csv",
+train_dataset = CheXpertDataset(data_path="data/CheXpert-v1.0-small/train.csv",
                           uncertainty_policy="zeros", transform=image_transformation)
 
-dataloader = DataLoader(dataset=dataset, batch_size=config["batch_size"], shuffle=True)
+valid_dataset = CheXpertDataset(data_path="data/CheXpert-v1.0-small/valid.csv",
+                          uncertainty_policy="zeros", transform=image_transformation)
+
+
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=config["batch_size"], shuffle=True)
+valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=config["batch_size"], shuffle=True)
 
 device = "cpu"
 
@@ -57,24 +62,24 @@ heart_model = load_segmentation_model(config["heart_model_path"], device)
 lung_model = load_segmentation_model(config["lung_model_path"], device)
 print(f"Starting segmentation on device {device}")
 
-num_images = len(dataloader)
 
-for step, (image, label) in enumerate(dataloader):
-    print(f"Segmenting image {step + 1} of {num_images}")
-    image_path = dataset.get_path(step)
-    directory = image_path.split("train")[-1].split("view")[0]
-    image_name = image_path.split("/")[-1]
-    image_name = image_name.split(".jpg")[0] +".png"
+for dataloader in (train_dataloader, valid_dataloader):
+    num_images = len(dataloader)
+    for step, (image, label) in enumerate(dataloader):
+        print(f"Segmenting image {step + 1} of {num_images}")
+        image_path = dataloader.dataset.get_path(step)
+        directory = image_path.split("view")[0].split("small")[-1]
+        image_name = image_path.split("/")[-1]
 
-    os.makedirs(heart_dir + directory, exist_ok=True)
-    os.makedirs(lung_dir + directory, exist_ok=True)
+        os.makedirs(heart_dir + directory, exist_ok=True)
+        os.makedirs(lung_dir + directory, exist_ok=True)
 
-    heart_pred = infer_from_tensor(image, heart_model, device)
-    lung_pred = infer_from_tensor(image, lung_model, device)
-    heart_mask = transforms.ToPILImage()(heart_pred).convert("1")
-    lung_mask = transforms.ToPILImage()(lung_pred).convert("1")
-    heart_mask.save(os.path.join(heart_dir + directory, image_name), "PNG")
-    lung_mask.save(os.path.join(lung_dir + directory, image_name), "PNG")
+        heart_pred = infer_from_tensor(image, heart_model, device)
+        lung_pred = infer_from_tensor(image, lung_model, device)
+        heart_mask = transforms.ToPILImage()(heart_pred).convert("1")
+        lung_mask = transforms.ToPILImage()(lung_pred).convert("1")
+        heart_mask.save(os.path.join(heart_dir + directory, image_name))
+        lung_mask.save(os.path.join(lung_dir + directory, image_name))
 
 print("Done")
 
